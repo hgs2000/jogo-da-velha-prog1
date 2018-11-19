8ad4f8dccb583e202ad7599437205ea805ff0c04
Henrique Starosky <hgs.true@gmail.com>
2018-11-19 21:50:26 -0200

    .

---
+++ JogoDaVelha/src/jogodavelha/Frontend/Inicio.java
@@ -0,0 +1,14 @@
+package jogodavelha.Frontend;
+
+import javax.swing.*;
+
+public class Inicio {
+    private JPanel panelInicial;
+    private JLabel LabelTitulo;
+    private JButton buttonInicioAI;
+    private JButton buttonInicio2Player;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
